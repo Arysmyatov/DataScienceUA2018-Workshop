32cb8d0bcac4a96a01eefc75abe375713699ff14
Andriy Arysmyatov <andriy.arysmyatov@outlook.com>
2018-03-19 18:08:38 +0200

    Changes for QNA

--- DataScienceUA2018/Dialogs/QnaDialog.cs
+++ DataScienceUA2018/Dialogs/QnaDialog.cs
@@ -4,7 +4,7 @@ using Microsoft.Bot.Builder.CognitiveServices.QnAMaker;
 namespace DataScienceUA2018.Dialogs
 {
     [Serializable]
-    [QnAMaker("7ac0cd1e62d347c3bf966e387a47fd14", "b5c4f1dd-4c60-4ecd-9f90-cd97dc68ef2c")]
+    [QnAMaker("0965c7390f26481a99eda14c21e8d5a0", "7a99d5e0-1cbd-4758-b4d5-89caf5a938d5")]
     public class QnaDialog : QnAMakerDialog
     {
     }
